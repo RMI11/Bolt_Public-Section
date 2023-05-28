0c626a3442ff428edff31f6efff7dfd0f09b73fe
RR <rajko.fed@gmail.com>
2023-05-28 16:08:43 +0200

    first commit

--- script.js
+++ script.js
@@ -44,3 +44,22 @@ themeButton.addEventListener("click", () => {
     });
     }
 });
+
+if (window.innerWidth < 1024) {
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("price_card");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}}
